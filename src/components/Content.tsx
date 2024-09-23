150ff3d763917e35aa2a88f42789af1e62f59885
cupid chakma <greendaycu20@gmail.com>
2024-09-23 22:01:06 +0600

    Update: Body Section

---
+++ src/components/Content.tsx
@@ -0,0 +1,15 @@
+const Content = () => {
+    return <>
+                <section className="__portfolio-body-section">
+                    <div className="section-group-1">
+                        <h1 className="bio-name">Hello, I&apos;m Cupid</h1>
+                        <h1 className="bio-description">Software Engineer</h1>
+                    </div>
+                    <div className="section-group-2">
+                        <h1 className="bio-name">Hello My Name Is Cupid Chakma</h1>
+                    </div>
+                </section>    
+           </>
+}
+
+export default Content;
---
+++ src/components/Header.tsx
@@ -0,0 +1,42 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faFacebook, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+
+const Header = () => {
+    const tabContents = [
+        'Home',
+        'About',
+        'Experience',
+        'Services'
+    ];
+    return <>
+        <div className="__portfolio-header">
+            <div className="__portfolio-block-1">
+                <ul className="__portfolio-content-group-1">
+                    {tabContents?.map((value, index) => {
+                        return <li className={`content-item ${value}`} key={index}><p>{value}</p></li>
+                    })}
+                </ul>
+            </div>
+            <div className="__portfolio-block-2">
+                <ul className="__portfolio-content-group-2">
+                    <li><FontAwesomeIcon
+                        icon={faGithub}
+                    /></li>
+                    <li>
+                        <FontAwesomeIcon
+                            icon={faFacebook}
+                        />
+                    </li>
+                    <li>
+                        <FontAwesomeIcon
+                            icon={faLinkedin}
+                        />
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </>
+}
+
+export default Header;
